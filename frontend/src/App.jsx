524575189cd2bc1e392bf3cf4125737c569a7abf
Code-Apprentice <codeguru42@gmail.com>
2026-02-01 10:54:40 -0700

    feat: integrate TournamentDataContext into App.jsx

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -3,6 +3,7 @@ import { BrowserRouter as Router, Navigate, Route, Routes } from 'react-router-d
 import Titlebar from './components/layout/Titlebar';
 import { NotificationProvider } from './components/NotificationContext';
 import { ThemeProvider } from './components/ThemeContext';
+import { TournamentDataProvider } from './components/TournamentDataContext';
 import AdminGamesView from './views/AdminGamesView';
 import AdminLayout from './views/AdminLayout';
 import AdminPlayersView from './views/AdminPlayersView';
@@ -12,19 +13,21 @@ function App() {
   return (
     <NotificationProvider>
       <ThemeProvider>
-        <Router>
-          <Box sx={{ flexGrow: 1 }}>
-            <Titlebar />
-            <Routes>
-              <Route path="/" element={<GameSubmissionView />} />
-              <Route path="/admin" element={<AdminLayout />}>
-                <Route index element={<Navigate to="games" replace />} />
-                <Route path="games" element={<AdminGamesView />} />
-                <Route path="players" element={<AdminPlayersView />} />
-              </Route>
-            </Routes>
-          </Box>
-        </Router>
+        <TournamentDataProvider>
+          <Router>
+            <Box sx={{ flexGrow: 1 }}>
+              <Titlebar />
+              <Routes>
+                <Route path="/" element={<GameSubmissionView />} />
+                <Route path="/admin" element={<AdminLayout />}>
+                  <Route index element={<Navigate to="games" replace />} />
+                  <Route path="games" element={<AdminGamesView />} />
+                  <Route path="players" element={<AdminPlayersView />} />
+                </Route>
+              </Routes>
+            </Box>
+          </Router>
+        </TournamentDataProvider>
       </ThemeProvider>
     </NotificationProvider>
   );
